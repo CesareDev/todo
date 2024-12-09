fa94e6b2b9f409014f3f3e88438aca1845a382d1
CesareDev <corsicesare.lavoro@gmail.com>
2024-12-09 15:27:28 +0100

    Start argument parsing

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # todo
-A todo command line tool writte in C 
+
+Command line tool written in `C` for personal uses.
+
+## Dependencies
+
+- `gcc`
+- `cmake`
+
+## Installation
+
+```bash
+git clone https://github.com/CesareDev/todo.git
+cd todo
+mkdir build
+cd build
+cmake ..
+make
+```
+
+## Usage
+
+The executable is located to the `build/release/todo/` or `build/debug/todo/` directory.
+
+## Limitations
+
+- The tool is not yet implemented.
+- For now works only for bash.
--- todo/src/main.c
+++ todo/src/main.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+static char* REMINDERS_FILE;
+
+void set_reminders_file_path() {
+    char *home_directory = getenv("HOME");
+    if (home_directory == NULL) {
+        printf("Error: HOME environment variable is not set\n");
+        exit(1);
+    }
+
+    char *reminders_path = strncat(home_directory, "/.todo", 7);
+
+    REMINDERS_FILE = reminders_path;
+}
+
+int add_reminder(char* reminder) {
+    return 0;
+}
+
+int delete_reminder(char* reminder_id) {
+    return 0;
+}
+
+int help_page(char* executable_name) {
+    printf("USAGE:\n");
+    printf("  %s [OPTIONS] [ARGS]\n\n", executable_name);
+    printf("OPTIONS:\n");
+    printf("  -a <reminders>\n");
+    printf("     Add <reminder> to the list of the reminders\n");
+    printf("  -d <index> [Not implemented yet]\n");
+    printf("     Remove the <index>-th reminder from the list\n");
+    return 0;
+}
 
 int main(int argc, char* argv[]) {
 
+    set_reminders_file_path();
+
     int opt;
-    while ((opt = getopt(argc, argv, "ha:c")) != -1)
+    while ((opt = getopt(argc, argv, "ha:d:")) != -1)
     {
         switch (opt) {
-            case 'h': printf("Usage: %s -a <argument>\n", argv[0]); return 0;
-            case 'a': printf("Argument: %s\n", optarg); return 0;
-            case 'c': printf("Option c\n"); return 0;
+            case 'h': return help_page(argv[0]);
+            case 'a': return add_reminder(optarg);
+            case 'd': return delete_reminder(optarg);
             default: printf("Type: %s -h for the help page\n", argv[0]); return 1;
         }
     }
